017b192abdb2c4800195759cc7696b36efba600b
JoshuaMills <Jbmills291@gmail.com>
2018-03-12 20:41:06 -0400

    setup files

--- app/src/main/java/com/mills/b/joshua/top10downloader/MainActivity.java
+++ app/src/main/java/com/mills/b/joshua/top10downloader/MainActivity.java
@@ -1,13 +1,86 @@
 package com.mills.b.joshua.top10downloader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting asyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        Log.d(TAG, "onCreate: done");
+
     }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is" + s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: Starts with  " + strings[0]);
+            String rssFeed = downloadXML(strings[0]);
+            if(rssFeed == null){
+                Log.e(TAG, "doInBackground: Error downloading");
+            }
+            return rssFeed;
+        }
+
+        private String downloadXML(String urlPath){
+            StringBuilder xmlResult = new StringBuilder();
+            try{
+                URL url =new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: The response is "+ response);
+                //InputStream inputStream = connection.getInputStream();
+                //InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                //BufferedReader reader = new BufferedReader(inputStreamReader);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                int charsRead;
+                char[] inputBuffer = new char[500];
+                while(true){
+                    charsRead = reader.read(inputBuffer);
+                    if(charsRead<0){
+                        break;
+                    }
+                    if(charsRead>0){
+                        xmlResult.append(String.copyValueOf(inputBuffer,0,charsRead));
+                    }
+                }
+                reader.close();
+                return xmlResult.toString();
+            }catch (MalformedURLException e){
+                Log.e(TAG, "downloadXML: MalformedURLException" + e.getMessage());
+            }catch (IOException e){
+                Log.e(TAG, "downloadXML: IOException" + e.getMessage());
+            }catch (SecurityException e){
+                Log.e(TAG, "downloadXML: SecurityException" + e.getMessage());
+            }
+
+            return null;
+        }
+
+    }
+
 }
